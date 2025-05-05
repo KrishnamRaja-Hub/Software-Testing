8abae6a6c46e1d9d6611b6e9c2075e8da80304df
Krishnam Raja <140960788+KrishnamRaja-Hub@users.noreply.github.com>
2025-05-05 20:36:24 +0530

    Delete test-Visual_Regression directory

--- test-Visual_Regression/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-URL = "https://the-internet.herokuapp.com/login"  # Replace with your target URL
-BROWSERS = ["chrome", "firefox"]  # Browsers to test
-SCREENSHOTS_DIR = "screenshots"
-DIFFS_DIR = "diffs"
-REPORT_PATH = "reports/diff_report.html"
